2df57bacc308f41efae99b4f3e3e2037a28fef11
reb <reb@172.20.10.3>
2022-10-28 15:18:57 +0200

    init commit

---
+++ provaIngsw/src/main/java/dapa/provaIngsw/FunnyAlgorithms.java
@@ -0,0 +1,13 @@
+package dapa.provaIngsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class FunnyAlgorithms 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ provaIngsw/src/test/java/dapa/provaIngsw/FunnyAlgorithmsTest.java
@@ -0,0 +1,38 @@
+package dapa.provaIngsw;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class FunnyAlgorithmsTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public FunnyAlgorithmsTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( FunnyAlgorithmsTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
